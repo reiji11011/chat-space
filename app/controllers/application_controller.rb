308cb69c484366b482299f1de3114e52406a6eee
reiji11011 <suga@sugamanoMacBook-Air.local>
2017-08-07 16:34:41 +0900

    パラメーターとして送られるnameを許可するメソッドの作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+  # sign_upのときに、nameも許可する
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:name])
+  #account_updateのときに、nameも許可する
+    devise_parameter_sanitizer.permit(:account_update, keys:[:name])
+  end
 end
