4845b35ed0eed89cffc5706b8ab8c0d88d3582a3
reiji11011 <suga@sugamanoMacBook-Air.local>
2017-08-07 16:27:41 +0900

    attr_accessorの位置の変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
+  attr_accessor :name
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  attr_accessor :name
 end
