20a1da343f00eb7bf3dbd7ccd8e2d1c5b59d7fcd
reiji11011 <suga@sugamanoMacBook-Air.local>
2017-08-12 19:00:22 +0900

    groupsのルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'groups#index'
-  resources :groups, only: :index do
+  resources :groups, only: [:index, :new, :edit] do
     resources :messages, only: :index
   end
   resources :users, only: [:edit, :update]
