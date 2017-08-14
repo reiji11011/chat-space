0f1aeb26af329bce49b0cf29857f8468ba1e7d2f
reiji11011 <suga@sugamanoMacBook-Air.local>
2017-08-15 05:23:43 +0900

    関連項目も含めて１度に保存、削除するための記述

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,6 @@
 class Group < ApplicationRecord
   has_many :groups_users, dependent: :destroy
   has_many :users, through: :groups_users
+
+  accepts_nested_attributes_for :groups_users, allow_destroy: true
 end
