84d6e4423fa80c0d7f63c62d5c0fdd01e0e120a4
reiji11011 <suga@sugamanoMacBook-Air.local>
2017-08-15 06:25:40 +0900

    フォームの内容をDBに保存するための記述

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,11 +7,19 @@ class GroupsController < ApplicationController
   end
 
   def create
+    Group.create(group_params)
+    redirect_to root_path
   end
 
   def edit
   end
 
   def upadate
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, { :user_ids => [] })
   end
 end
