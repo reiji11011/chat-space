2321d467c0168a63a8b00b626b7a7270b487d731
reiji11011 <suga@sugamanoMacBook-Air.local>
2017-08-15 06:55:28 +0900

    グループ名が空の時、条件分岐を行うための記述

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,8 +7,12 @@ class GroupsController < ApplicationController
   end
 
   def create
-    Group.create(group_params)
-    redirect_to root_path
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path
+    else
+      render action: :new
+    end
   end
 
   def edit
