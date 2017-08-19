171fed8f5dffca7fe6514a99c8998507eee49026
reiji11011 <suga@sugamanoMacBook-Air.local>
2017-08-20 03:17:18 +0900

    削除機能は必要ないため、allow_destroyオプションの記述の削除

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,7 +2,7 @@ class Group < ApplicationRecord
   has_many :groups_users, dependent: :destroy
   has_many :users, through: :groups_users
 
-  accepts_nested_attributes_for :groups_users, allow_destroy: true
+  accepts_nested_attributes_for :groups_users
 
   validates :name, presence: true
 end
