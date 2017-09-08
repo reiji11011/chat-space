5df1a178f35aadf44429043dbc66f035e535d921
reiji11011 <suga@sugamanoMacBook-Air.local>
2017-09-08 16:14:22 +0900

    Groupとmessageのアソシエーションの追加

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,7 @@
 class Group < ApplicationRecord
   has_many :groups_users, dependent: :destroy
   has_many :users, through: :groups_users
+  has_many :messages
 
   accepts_nested_attributes_for :groups_users
 
