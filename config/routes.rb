782195b135a0b5d79e0201f8db245436985c0900
reiji11011 <suga@sugamanoMacBook-Air.local>
2017-09-08 12:18:08 +0900

    Createアクションの追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'groups#index'
   resources :groups, only: [:index, :new, :create, :edit, :update] do
-    resources :messages, only: :index
+    resources :messages, only: [:index, :create]
   end
   resources :users, only: [:edit, :update]
 end
