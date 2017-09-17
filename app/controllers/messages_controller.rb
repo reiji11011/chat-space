943dbcf6f608cfe385c79006359d44f65d9abfc3
reiji11011 <suga@sugamanoMacBook-Air.local>
2017-09-17 19:54:10 +0900

    Index,createアクションの前に初期値を持たせるメソッドを動かす記述

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,6 @@
 class MessagesController < ApplicationController
+  before_action :initial_value, only: [:index, :create]
+
   def index
     @group = Group.find(params[:group_id])
     @groups = current_user.groups
